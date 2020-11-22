c2041dd92c56a44917f2b45176773898708b93a0
ugochukwuomeje <ugochukwuomeje1@gmail.com>
2020-11-22 17:29:18 +0100

    create my_each logic

--- enumerables.rb
+++ enumerables.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: false
 
 module Enumerables
+  #-------------my each method----------------#
   def my_each
     # your code here
+    for x in tools
+      yield(x)
+    end
   end
+
 end
