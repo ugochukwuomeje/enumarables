609dcf69e0d8f9ddd9824d345013ba3efbd1acf5
ugochukwuomeje <ugochukwuomeje1@gmail.com>
2020-11-29 18:42:30 +0100

    create none method

--- enumerables.rb
+++ enumerables.rb
@@ -3,16 +3,23 @@
 module Enumerables
   #-------------my each method----------------#
   def my_each
+    return to_enum(:my_each) unless block_given?
     # your code here
-    for x in tools
+    x = self.to_a if self.class? == Array
+    x = self.to_a if self.class? ==  Range
+
+    for x in self
       yield(x)
     end
   end
 
   #----------------my each with index----------#
   def my_each_with_index
+    return to_enum(:my_each) unless block_given?
+    x = self.to_a if self.class? == Array
+    x = self.to_a if self.class? ==  Range
     count = 0;
-    for x in tools
+    for x in self
       yield(count, x)
       count = count +1
     end 
@@ -20,8 +27,11 @@ module Enumerables
 
   #----------------#my_select-------------------#
   def my_select
+    return to_enum(:my_each) unless block_given?
+    x = self.to_a if self.class? == Array
+    x = self.to_a if self.class? ==  Range
     result = []
-    for x in tools
+    for x in self
       if(yield(x))
         result << x
       end
@@ -31,8 +41,11 @@ module Enumerables
   
   #-------------------#my_all---------------------#
   def my_all
+    return to_enum(:my_each) unless block_given?
+    x = self.to_a if self.class? == Array
+    x = self.to_a if self.class? ==  Range
     result = true
-    for x in tools
+    for x in self
       if(!yield(x))
         result = false
       end
@@ -42,8 +55,11 @@ module Enumerables
 
   #-------------------#my_any---------------------#
   def my_any
+    return to_enum(:my_each) unless block_given?
+    x = self.to_a if self.class? == Array
+    x = self.to_a if self.class? ==  Range
     result = false
-    for x in tools
+    for x in self
       if(yield(x))
         result = true
       end
@@ -51,6 +67,20 @@ module Enumerables
     result
   end
 
-  
+  #-------------------#my_any---------------------#
+  def my_none
+    return to_enum(:my_each) unless block_given?
+    x = self.to_a if self.class? == Array
+    x = self.to_a if self.class? ==  Range
+    result = true
+    for x in self
+      if(yield(x))
+        result = false
+      end
+    end
+    result
+  end
 
 end
+
+[2,3,5,8].my_each {|numbers| puts numbers}
---
+++ testclass.rb
@@ -0,0 +1,5 @@
+(1..8).each_with_index do |x, y|
+
+print "The value is #{x}"
+p " the index is #{y}"
+end
